bcf8f1b1e056c72bc6386e02aaabc7551c1b3dd0
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-01 20:47:32 +0000

    another commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Simple Shell
 
 <p align ="center">
+
 THE WAY TO SUCCESS
+
+</p>
+
+<p align ="center">
 
 ![alt text](https://miro.medium.com/max/1204/1*5w2fOfCVL5FdK7oYDJgIHg.png)
 
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#endif /* _SHELL_H_ */
