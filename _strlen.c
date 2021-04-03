4e4ee624436263dbe6647023faee9bb980b2e0fe
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-03 20:35:35 +0000

    new adds

---
+++ _strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _strlen - size of string
+* @str: string
+* Return: size of string
+*/
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+	{
+	}
+	return (i);
+}
