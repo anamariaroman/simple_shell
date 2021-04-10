0b51344ea37a67a3ae8d0893643e096a813280b8
anamariaroman <2979@holbertonschool.com>
2021-04-09 20:04:18 -0500

    task

---
+++ _strcpy.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+* _strcpy - 
+*
+*
+*/
+void _strcpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = src[i];
+}
