69447ef8a42aac8ed6679b02230bcd71cf361fbf
anamariaroman <2979@holbertonschool.com>
2021-04-09 20:04:53 -0500

    task

---
+++ exit.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+* _exit - read CTRL-D command
+* @command: command
+* @controller: size of command
+*/
+void _exit(char *command, ssize_t controller)
+{
+	if (_strcmp(line,"exit",0,3))
+	{
+		if (controller == EOF)
+			write(1, "\n", 1);
+		exit(0)
+	}
+	if (controller == EOF)
+	{
+		write(1, "\n",1);
+		exit(0);
+	}
+}
