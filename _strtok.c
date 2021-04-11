6e3a2efdedaacd915fc670ef7d2cf7cb67d5d1be
anamariaroman <2979@holbertonschool.com>
2021-04-11 01:00:28 -0500

    token

---
+++ _strtok.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * _strtok - find the funcion and returns the value
+ *
+*/
+
+char **_strtok(char *j)
+{
+	unsigned int i;
+	const unsigned int len = _strlen(j);
+	char *token;
+	char **tokens = NULL;
+
+	tokens = malloc(len * sizeof(char *));
+	{
+		if (tokens == NULL)
+		return (NULL);
+	}
+
+	token = strtok(j, " \t\r\n");
+
+	i = 0;
+	while (token)
+	{
+		tokens[i] = malloc(_strlen(token) * sizeof(char));
+		if (tokens == NULL)
+			return (NULL);
+
+		tokens[i] = token;
+
+		token = strtok(NULL, " \t\r\n");
+		i++;
+	}
+
+	tokens[i] = NULL;
+
+	return (tokens);
+}
