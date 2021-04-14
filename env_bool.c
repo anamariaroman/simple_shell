d42790ad9cf137635830b1009e7a2b80abc4b8fc
anamariaroman <2979@holbertonschool.com>
2021-04-13 19:50:45 -0500

    bool

---
+++ env_bool.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * env_bool - controls the built-in env command
+ * @s: string
+ * @env: is the env of the main ()
+ * Return: true if this is the built-in env command
+*/
+bool env_bool(char *s, char *env[])
+{
+	if (_strcmp(s, "env", 0, 2))
+	{
+		p_env(env);
+		return (true);
+	}
+
+	return (false);
+}
