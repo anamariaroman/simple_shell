a3eef893efaff9a58ba0e5770c99cc5d7661908b
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-15 06:53:28 +0000

    count and delete

--- _exit_.c
+++ _exit_.c
@@ -6,6 +6,7 @@
 */
 void _exit_(char *command, ssize_t status)
 {
+	/* ctrl-d */
 	if (status == -1)
 	{
 		free(command);
@@ -13,6 +14,7 @@ void _exit_(char *command, ssize_t status)
 		_putchar('\n');
 		exit(0);
 	}
+	/* exit comand */
 	if (_strcmp(command, "exit", 0, 3))
 	{
 		free(command);
