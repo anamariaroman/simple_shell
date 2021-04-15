e0e95c4e7864e2569a0b2787ff4a6eddaaecbe96
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-04-15 14:37:52 +0000

    delete

--- _strtok.c
+++ _strtok.c
@@ -1,11 +1,31 @@
 #include "shell.h"
+/**
+* cletter - count
+* @letters: sentences
+* Return: return num of letters or words
+*/
+int cletter(char *letters)
+{
+	int i = 0;
+	int l = 0;
 
+	while (letters[i])
+	{
+		if (letters[i] == 32 && letters[i + 1] != 32 && letters[i + 1] != 0)
+		{
+			l++;
+		}
+		i++;
+	}
+	if (letters[0] != 32)
+		l++;
+	return (l);
+}
 /**
  * _strtok - find the funcion and returns the value
  * @j: string to make token
  * Return: num of tokens
 */
-
 char **_strtok(char *j)
 {
 	unsigned int i;
